b51ce96024ea6748fc2e25ab3e1602004bd62162
vinicostaa <maykell.costa@icloud.com>
2020-05-27 00:04:19 -0300

    Implements Patterns: Repository, DependencyInjection and UnitOfWork

---
+++ src/Infrastructure/Repository/DapperSqlPaymentRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using ApplicationCore.Entities;
+using ApplicationCore.Interfaces;
+using Dapper;
+
+namespace Infrastructure.Repository
+{
+    public class DapperSqlPaymentRepository : DapperSqlRepository<Payment>, IPaymentRepository
+    {
+        public DapperSqlPaymentRepository()
+        {
+            TableName = "Payment";
+        }
+    }
+}
---
+++ src/WepApi/Controllers/ProductController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApplicationCore.Entities;
+using ApplicationCore.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WepApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private readonly ILogger<ProductController> _logger;
+        private readonly IProductRepository _productRepository;
+
+        public ProductController(ILogger<ProductController> logger, IProductRepository productRepository)
+        {
+            _logger = logger;
+            _productRepository = productRepository;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var product = new Product();
+            _productRepository.InsertProduct(product);
+            return Ok();
+        }
+    }
+}
